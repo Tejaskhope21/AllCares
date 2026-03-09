66913ac601a82285568aee970c7c751e884137ae
tejaskhope21 <tejaskhope600@gmail.com>
2026-03-09 13:59:07 +0530

    remove the backend folder

--- ecommerce/ecommerce/src/main/java/com/ssinfotech/ecommerce/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssinfotech.ecommerce.repository;
-
-import com.ssinfotech.ecommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Custom query methods can be added here if needed in the future
-}
